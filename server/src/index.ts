9d80ecb8cd9b475d6517dab932db3d4be4b41596
NIGHTCODE21 <priyanshipankhaniya21@gmail.com>
2026-03-19 10:30:46 +0530

    Refactor socket handling and remove user tracking
    Removed user management logic and socket event handlers.

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import http from "http";
 import cors from "cors";
 import { Server } from "socket.io";
+import { socketHandler } from "./socket/socket";
 
 const app = express();
 app.use(cors());
@@ -9,51 +10,10 @@ app.use(cors());
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: {
-    origin: "*",
-  },
+  cors: { origin: "*" },
 });
 
-interface Users {
-  [key: string]: string;
-}
-
-const users: Users = {};
-
-io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
-
-  socket.on("join", (name: string) => {
-    users[socket.id] = name;
-
-    socket.emit("message", {
-      user: "Admin",
-      text: `Welcome ${name}`,
-    });
-
-    socket.broadcast.emit("message", {
-      user: "Admin",
-      text: `${name} joined`,
-    });
-  });
-
-  socket.on("sendMessage", (text: string) => {
-    io.emit("message", {
-      user: users[socket.id],
-      text,
-    });
-  });
-
-  socket.on("disconnect", () => {
-    const name = users[socket.id];
-    delete users[socket.id];
-
-    socket.broadcast.emit("message", {
-      user: "Admin",
-      text: `${name} left`,
-    });
-  });
-});
+socketHandler(io);
 
 server.listen(5000, () => {
   console.log("Server running on port 5000");
